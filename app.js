e4742a88b31422c7aa103cb7a710bce7e3fe0481
Adil Ch <adil@gmail.com>
2022-03-26 12:28:38 +0500

    third

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 const log = console.log;
-const express = require('express');
+const express = require("express");
 const app = express();
-const server = app.listen(4000, () =>
+const server = app.listen(process.env.PORT || 5000, () =>
   log(`Realtime Orderbook started on port 4000`)
 );
 
 //Expres Server
 app.use(express.json());
-app.use('/serveapi', require('./Routes/serveapi'));
-app.use('/', require('./Routes/pages'));
-app.get('/', (_, res) => {
-  res.redirect('/home');
+app.use("/serveapi", require("./Routes/serveapi"));
+app.use("/", require("./Routes/pages"));
+app.get("/", (_, res) => {
+  res.redirect("/home");
 });
 
 //Dependent Modules
-const binancews = require('./Utils/binancews');
+const binancews = require("./Utils/binancews");
 
 //Socket
-const socketio = require('socket.io');
+const socketio = require("socket.io");
 const io = socketio(server);
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   log(`New socket connection established with id ${socket.id}`);
-  socket.on('SYMBOL', (payload) => {
+  socket.on("SYMBOL", (payload) => {
     binancews.switchSymbol(payload);
   });
 });
 
 //Event Emitter
-binancews.EE.on('OBUPDATES', (payload) => {
-  io.emit('OBUPDATES', payload);
+binancews.EE.on("OBUPDATES", (payload) => {
+  io.emit("OBUPDATES", payload);
 });
